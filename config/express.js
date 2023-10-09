4d68094fc623233865aed615e0751e9ddd1563e4
Lucas Sapienza <sapienzalucas@gmail.com>
2023-10-09 10:09:43 -0300

    initial commit

---
+++ config/express.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const config = require("config");
+
+module.exports = () => {
+  const app = express();
+
+  app.set("port", process.env.PORT || config.get("server.port"));
+
+  // MIDDLEWARES
+  app.use(bodyParser.json());
+
+  return app;
+};
