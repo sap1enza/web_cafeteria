138e06c07f9073cc4e44148f13c10438611afc52
Lucas Sapienza <sapienzalucas@gmail.com>
2023-10-09 11:01:39 -0300

    implement create category endpoint

--- api/controllers/categories.js
+++ api/controllers/categories.js
@@ -1,8 +1,21 @@
+const uuidv4 = require("uuid/v4");
+
 module.exports = (app) => {
   const categoriesDB = app.data.categories;
   const controller = {};
 
+  const { categories: categoriesMock } = categoriesDB;
+
   controller.listCategories = (req, res) => res.status(200).json(categoriesDB);
+
+  controller.saveCategory = (req, res) => {
+    categoriesMock.data.push({
+      id: uuidv4(),
+      name: req.body.name,
+    });
+
+    res.status(201).json(categoriesMock);
+  };
 
   return controller;
 };
--- api/routes/categories.js
+++ api/routes/categories.js
@@ -1,5 +1,8 @@
 module.exports = (app) => {
   const controller = app.controllers.categories;
 
-  app.route("/api/v1/categories").get(controller.listCategories);
+  app
+    .route("/api/v1/categories")
+    .get(controller.listCategories)
+    .post(controller.saveCategory);
 };
