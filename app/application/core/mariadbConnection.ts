01511cecf03d76c9158dfd21f2d315f04d2b8d76
Bruno Blauzius <you@example.com>
2023-10-21 17:49:36 -0300

    adicionado a criação da tabela categories

--- app/application/core/mariadbConnection.ts
+++ app/application/core/mariadbConnection.ts
@@ -19,14 +19,23 @@ class MysqlConnection
         });
         console.log("SUCCESS:", `Conexão estabelecida host: ${process.env.MARIADB_HOST}:${process.env.MARIADB_PORT}`);
         
-        await this.connection.query(`CREATE TABLE IF NOT EXISTS customers (
-            id INT PRIMARY KEY AUTO_INCREMENT,
-            name VARCHAR(200) not null,
-            email VARCHAR(245) not null unique,
-            cpf_cnpj VARCHAR(20) not null unique,
-            created datetime null,
-            modified datetime null
-        )  ENGINE=INNODB;`);
+        await this.connection.query(`
+            CREATE TABLE IF NOT EXISTS customers (
+                id INT PRIMARY KEY AUTO_INCREMENT,
+                name VARCHAR(200) not null,
+                email VARCHAR(245) not null unique,
+                cpf_cnpj VARCHAR(20) not null unique,
+                created datetime null,
+                modified datetime null
+            )  ENGINE=INNODB;
+
+            CREATE TABLE IF NOT EXISTS categories (
+                id INT PRIMARY KEY AUTO_INCREMENT,
+                name VARCHAR(200) not null,
+                created datetime null,
+                modified datetime null
+            )  ENGINE=INNODB;
+        `);
     }
 
     public conn() {
