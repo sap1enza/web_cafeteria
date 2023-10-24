e0b39f7b457a6514f04cb76332ab3ea3c92e14a8
Lucas Sapienza <sapienzalucas@gmail.com>
2023-10-23 21:05:50 -0300

    implement order and order_products

---
+++ app/application/api/controllers/PedidoProdutosController.ts
@@ -0,0 +1,133 @@
+import * as HttpStatus from 'http-status';
+import PedidoProdutosRepository from "../../repositories/PedidoProdutosRepository";
+import PedidoRepository from "../../repositories/PedidoRepository";
+import ProdutoRepository from "../../repositories/ProdutoRepository";
+import ResponseAPI from '../../core/ResponseAPI';
+import MysqlDataBase from '../../database/MysqlDataBase';
+import PedidoProduto from '../../../domain/entity/pedido_produto';
+
+class PedidoProdutosController {
+    /**
+     *
+     */
+    public repository: PedidoProdutosRepository;
+    public pedidoRepository: PedidoRepository;
+    public produtoRepository: ProdutoRepository;
+
+    /**
+     *
+     */
+    constructor() {
+        this.repository = new PedidoProdutosRepository(new MysqlDataBase());
+        this.pedidoRepository = new PedidoRepository(new MysqlDataBase());
+        this.produtoRepository = new ProdutoRepository(new MysqlDataBase());
+    }
+
+    /**
+     *
+     * @param request
+     * @param response
+     */
+    public all = async (request, response) => {
+        try {
+            let data = await this.repository.getAll(request.query);
+            response.status(HttpStatus.OK).json(ResponseAPI.list(data));
+        } catch(err) {
+            response.status(HttpStatus.BAD_REQUEST).json(ResponseAPI.error(err.message));
+        }
+    }
+
+    /**
+     *
+     * @param request
+     * @param response
+     */
+    public store = async (request, response) => {
+        try {
+            let order = await this.pedidoRepository.findById(request.body.order_id);
+            let product = await this.produtoRepository.findById(request.body.product_id);
+
+            let order_product = new PedidoProduto(
+                order,
+                product
+            );
+
+            try {
+                let data = await this.repository.store(order_product);
+                response.status(HttpStatus.OK).json(ResponseAPI.data(data));
+            } catch(err) {
+                response.status(HttpStatus.INTERNAL_SERVER_ERROR).json(ResponseAPI.error(err.message));
+            }
+        } catch (err) {
+            response.status(HttpStatus.BAD_REQUEST).json(ResponseAPI.error(err.message));
+        }
+    }
+
+    /**
+     *
+     * @param request
+     * @param response
+     */
+    public update = async (request, response) => {
+        try {
+            let order = await this.pedidoRepository.findById(request.body.order_id);
+            let product = await this.produtoRepository.findById(request.body.product_id);
+
+            let order_product = new PedidoProduto(
+              order,
+              product
+            );
+
+            let data = await this.repository.update(order_product, request.params.id);
+            response.status(HttpStatus.OK).json(ResponseAPI.data(data));
+        } catch (err) {
+            response.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .json(
+                ResponseAPI.error(err.message)
+            );
+        }
+    }
+
+    /**
+     *
+     * @param request
+     * @param response
+     */
+    public show = async (request, response) => {
+        try {
+            if (typeof request.params.id == 'undefined') {
+                response.status(HttpStatus.BAD_REQUEST).json(ResponseAPI.inputError("id", "ID do registro é requerido."));
+            }
+            let data = await this.repository.findById(request.params.id);
+            response.status(HttpStatus.OK).json(ResponseAPI.data(data));
+        } catch (err) {
+            response.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .json(
+                ResponseAPI.error(err.message)
+            );
+        }
+    }
+
+    /**
+     *
+     * @param request
+     * @param response
+     */
+    public delete = async (request, response) => {
+        try {
+            if (typeof request.params.id == 'undefined') {
+                response.status(HttpStatus.BAD_REQUEST).json(ResponseAPI.inputError("id", "ID do registro é requerido."));
+            }
+            let data = await this.repository.delete(request.params.id);
+            response.status(HttpStatus.NO_CONTENT).json({});
+        } catch (err) {
+            response.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .json(
+                ResponseAPI.error(err.message)
+            );
+        }
+    }
+
+}
+
+export default new PedidoProdutosController();
