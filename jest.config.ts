15dadc242c81f0989c95afcf6c641dcbf33a1797
arianemadureira <arianemadureira@live.com>
2023-10-23 23:14:11 -0300

    Fix Jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,10 @@
-import type {Config} from '@jest/types';
-// Sync object
-const config: Config.InitialOptions = {
-  verbose: true,
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+export default {
+  preset: 'ts-jest', // Define o preset para TypeScript
+  testEnvironment: 'node', // Define o ambiente de teste (Node.js)
+  testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.[jt]sx?$', // Padrão para encontrar arquivos de teste
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'], // Extensões de arquivos a serem consideradas
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    '^.+\\.tsx?$': 'ts-jest', // Transforma arquivos TypeScript
   },
   transformIgnorePatterns: ['./node_modules'],
 };
-module.exports = config;
